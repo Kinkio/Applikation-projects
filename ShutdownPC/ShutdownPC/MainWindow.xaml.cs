461c7f2f7bb40efceaea945974b3d9b01850ca3d
Anders <anderskink@gmail.com>
2018-02-20 15:18:53 +0100

    Ready to put on git.

---
+++ ShutdownPC/ShutdownPC/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShutdownPC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WriteToCmd cmd = new WriteToCmd();
+       
+        string message = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            desLabel.Content = @"Enter time for your computer to close."+ Environment.NewLine + "Set 0 instead of empty";
+            message = cmd.ReadFile();
+            MessageBack.Text = message;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int chosenHour = 0;
+            int chosenMin = 0;
+
+            try
+            {
+                chosenHour = int.Parse(txtboxHour.Text);
+                chosenMin = int.Parse(txtboxMin.Text);
+
+                message = "\n" + DateTime.Now.ToLongDateString() + "  Shutdown computer in: " + chosenHour.ToString() + " Hour and " + chosenMin.ToString() + " Min";
+              
+                new WriteToCmd(chosenHour, chosenMin, message);
+                MessageBack.Text += message;
+
+                btnSave.Visibility = Visibility.Hidden;
+                btnTurnback.Visibility = Visibility;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("You have not set any hours or min");
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnTurnback_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                cmd.disconnectommand();
+                message = "\n" + DateTime.Now.ToLongDateString() + "  Shutdown have been canceled";
+                new WriteToCmd(message);
+                MessageBack.Text += "\n" + DateTime.Now.ToLongDateString() + "  Shutdown have been canceled";
+
+                btnSave.Visibility = Visibility;
+                btnTurnback.Visibility = Visibility.Hidden;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ ShutdownPC/ShutdownPC/WriteToCmd.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShutdownPC
+{
+    class WriteToCmd
+    {
+        private int hour { get; set; }
+        private int min { get; set; }
+
+        private System.Diagnostics.Process process = new System.Diagnostics.Process();
+        private System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+
+        public WriteToCmd() { }
+
+        public WriteToCmd(string message)
+        {
+            WriteFile(message);
+        }
+
+        public WriteToCmd(int hour, int min, string message)
+        {
+            this.hour = hour;
+            this.min = min;
+            int result = ((60 * 60) * hour) + (60 * min);
+            sentCommand(result);
+            WriteFile(message);
+        }
+
+        private void sentCommand(int time)
+        {
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -s -t " + time.ToString();
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        public void disconnectommand()
+        {
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown -a";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        public void WriteFile(string message)
+        {
+            using (StreamWriter fileStream = File.AppendText("LogFile.txt"))
+            {
+                fileStream.WriteLine(message);
+            }
+        }
+
+        public string ReadFile()
+        {
+            string message = "";
+            using (StreamReader r = File.OpenText("LogFile.txt"))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {       
+                        message += Environment.NewLine + line; 
+                }
+                return message;
+            }
+        }
+    }
+}
