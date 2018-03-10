2d14dbb66eeedea310ace26dcaa1fda052766744
Anders <anderskink@gmail.com>
2018-03-10 11:44:56 +0100

    test page

---
+++ website/website/Scripts/script.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(".nav li a, .navbar-brand").click(function (event) {
+        event.preventDefault();
+        var href = this.href;
+        $(".bgArrowLeft, .page").animate({
+            left: '2000pt'
+        }, 1200,
+            function () {
+                window.location = href;
+            });
+
+    });
+});
+
