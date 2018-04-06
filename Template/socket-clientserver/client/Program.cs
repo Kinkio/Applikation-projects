caa602f6cbce30f1e719a137740fc27e7469fe30
Anders <anderskink@gmail.com>
2018-04-06 11:57:24 +0200

    add template of client server socket

---
+++ Template/socket-clientserver/client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const int portnumber = 8080;
+        const string serverip = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            //---data to send to the server---
+            string textToSend = DateTime.Now.ToString();
+
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(serverip, portnumber);
+            NetworkStream nwStream = client.GetStream();
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+            //---send the text---
+            Console.WriteLine("Sending : " + textToSend);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+            //---read back the text---
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
---
+++ Template/socket-clientserver/socket-clientserver/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace socket_clientserver
+{
+
+    class Program
+    {
+        const int portnumber = 8080;
+        const string serverip = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(serverip);
+            TcpListener listener = new TcpListener(localAdd, portnumber);
+            Console.WriteLine("Connecting...");
+            listener.Start();
+
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            //---read incoming stream---
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            //---convert the data received into a string---
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+
+            //---write back the text to the client---
+            Console.WriteLine("Sending back : " + dataReceived);
+            nwStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
+}
